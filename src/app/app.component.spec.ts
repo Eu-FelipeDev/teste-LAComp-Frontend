1454678106c9b936f012bf7c74ec3e5ef3190a71
Eu-FelipeDev <fls@ic.ufal.br>
2023-06-06 00:50:56 -0300

    Initial Dev

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'teste-LAComp-Frontend'`, () => {
+  it(`should have as title 'wheatherApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('teste-LAComp-Frontend');
+    expect(app.title).toEqual('wheatherApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('teste-LAComp-Frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('wheatherApp app is running!');
   });
 });
